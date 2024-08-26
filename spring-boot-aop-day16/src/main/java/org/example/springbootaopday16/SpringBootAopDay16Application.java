c8cdf11209b7f65d22f3140a471d2069fb4290b1
Senthil Kumar <senthil@striim.com>
2024-08-26 12:10:57 +0530

    spring aop

---
+++ spring-boot-aop-day16/src/main/java/org/example/springbootaopday16/SpringBootAopDay16Application.java
@@ -0,0 +1,13 @@
+package org.example.springbootaopday16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopDay16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAopDay16Application.class, args);
+    }
+
+}
---
+++ spring-boot-aop-day16/src/test/java/org/example/springbootaopday16/SpringBootAopDay16ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootaopday16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAopDay16ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
